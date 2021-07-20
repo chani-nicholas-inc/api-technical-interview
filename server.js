9101b214fd0995b5e20726c346a8731b30204111
milcahhalili <halilicenteno@gmail.com>
2021-07-20 15:24:04 -0700

    creates first url pattern

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 
+// get
 app.get('/', (req, res) => {
   res.send('Sup, Stars?')
 })
+
+app.get('/transits', (req, res) => {
+  res.send('This is the transits route.')
+})
+
+// post
+
+// put
+
+// delete
 
 app.listen(8000)
