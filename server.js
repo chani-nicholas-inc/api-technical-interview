f68488367c7271ab246d5dba653a6dce68ce0df6
milcahhalili <halilicenteno@gmail.com>
2021-07-20 15:28:42 -0700

    creates route that receives param

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app.get('/', (req, res) => {
   res.send('Sup, Stars?')
 })
 
+// url pattern
 app.get('/transits', (req, res) => {
   res.send('This is the transits route.')
+})
+
+// params
+
+app.get('/:param', (req, res) => {
+  console.log('req.params: ', req.params)
+  res.send(`This is the param: ${req.params.param}.`)
 })
 
 // post
