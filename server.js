abb59df9c18b708a8243b185004e5724da655e63
milcahhalili <halilicenteno@gmail.com>
2021-07-20 16:56:26 -0700

    creates POST route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
+// body-parser middleware
+app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+
 // get
 app.get('/', (req, res) => {
   res.send('Sup, Stars?')
@@ -15,7 +19,6 @@ app.get('/transits', (req, res) => {
 // params
 
 app.get('/p/:param', (req, res) => {
-  console.log('req.params: ', req.params)
   res.send(`This is the param: ${req.params.param}`)
 })
 
@@ -24,11 +27,28 @@ app.get('/p/:param', (req, res) => {
 app.get('/houses', (req, res) => {
   let houses = fs.readFileSync('./houses.json')
   let houseData = JSON.parse(houses)
-  console.log(houseData)
   res.send(houseData)
 })
 
+// list all user birth data sets
+app.get('/user-birth-data', (req, res) => {
+  let userBirthData = fs.readFileSync('./user-birth-data.json')
+  let data = JSON.parse(userBirthData)
+  res.send(data)
+})
+
 // post
+app.post('/user-birth-data', (req, res) => {
+  // read user birth data file
+  let userBirthData = fs.readFileSync('./user-birth-data.json')
+  let data = JSON.parse(userBirthData)
+  // add item to user birth data array
+  data.push(req.body)
+  // save user birth data to user-birth-data.json file
+  fs.writeFileSync('./user-birth-data.json', JSON.stringify(data))
+  // list all user birth data
+  res.redirect('/user-birth-data')
+})
 
 // put
 
