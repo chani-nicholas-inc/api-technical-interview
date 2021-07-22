7058b66dea71473cb0d23f21b76659586818f32e
Milcah <code@milcah.dev>
2021-07-22 10:54:56 -0700

    remove solution code

--- server.js
+++ server.js
@@ -3,70 +3,27 @@ const app = express()
 const fs = require('fs')
 
 // body-parser middleware
-app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
 
-// get
-app.get('/', (req, res) => {
-  res.send('Sup, Stars?')
-})
 
-// url pattern
-app.get('/transits', (req, res) => {
-  res.send('This is the transits route.')
-})
+// get home
 
-// params
 
-app.get('/p/:param', (req, res) => {
-  res.send(`This is the param: ${req.params.param}`)
-})
+// create transits url pattern
+
+
+// create route that takes in a param
+
 
 // list all houses
 
-app.get('/houses', (req, res) => {
-  let houses = fs.readFileSync('./houses.json')
-  let houseData = JSON.parse(houses)
-  res.send(houseData)
-})
 
 // get one house
-app.get('/houses/:idx', (req, res) => {
-  // get houses
-  let houses = fs.readFileSync('./houses.json')
-  let houseData = JSON.parse(houses)
 
-  // get arr index from url param
-  let houseIdx = parseInt(req.params.idx)
 
-  let result = {house: houseData[houseIdx]}
+// list all user birth data
 
-  // send data of the specified house
-  res.send(result)
-})
 
-// list all user birth data sets
-app.get('/user-birth-data', (req, res) => {
-  let userBirthData = fs.readFileSync('./user-birth-data.json')
-  let data = JSON.parse(userBirthData)
-  res.send(data)
-})
+// post new user birth data to user-birth-data.json
 
-// post
-app.post('/user-birth-data', (req, res) => {
-  // read user birth data file
-  let userBirthData = fs.readFileSync('./user-birth-data.json')
-  let data = JSON.parse(userBirthData)
-  // add item to user birth data array
-  data.push(req.body)
-  // save user birth data to user-birth-data.json file
-  fs.writeFileSync('./user-birth-data.json', JSON.stringify(data))
-  // list all user birth data
-  res.redirect('/user-birth-data')
-})
-
-// put
-
-// delete
 
 app.listen(8000)
